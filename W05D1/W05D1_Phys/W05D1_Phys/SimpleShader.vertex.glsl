b2391ec0cb70c0d42a625d3508eeff971af16fee
Michael Feeney <michael.feeney@badmonster.com>
2014-09-29 22:14:24 -0400

    Week 5, Day 1
    These are from the Physics 1 class, even though it really looks like
    more of a "graphics" topic - adding a uniform value. The intent was to
    get you to think of how you would add some sort of "graphical debug"
    sort of view to your physics code.

---
+++ W05D1/W05D1_Phys/W05D1_Phys/SimpleShader.vertex.glsl
@@ -0,0 +1,24 @@
+#version 400
+
+// in and out mean vertex buffer
+layout(location=0) in vec4 in_Position;  
+layout(location=1) in vec4 in_Color;
+out vec4 ex_Color;
+
+uniform mat4 ModelMatrix;    // 4 x 4 or 16x 32 bit floats
+uniform mat4 ViewMatrix;
+uniform mat4 ProjectionMatrix;
+// Add a colour value here
+uniform vec4 objectColour;		// our new variable (0,0,0,1)
+
+
+void main(void)
+{
+  mat4 OneMatrixToRuleThemAll = ProjectionMatrix * ViewMatrix * ModelMatrix;
+
+  // gl_Position is a built-in variable
+  gl_Position = OneMatrixToRuleThemAll * in_Position;
+
+  //ex_Color = in_Color;
+  ex_Color = objectColour;		// Our change
+}
